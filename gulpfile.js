6af18a8b74b9887735b5f516c73880e9b6b2f6b5
Rudolf <rudolf_bast@live.com>
2015-08-15 19:43:08 +0700

    fixed typos and folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
-    gulp.src('src/sass/**.*.scss')
-        .pipe(sass().on('error'), sass.logError)
+    gulp.src('src/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('assets/css/'));
 });
 
 gulp.task('default', function() {
-    gulp.watch('sass/**/*.scss', ['styles']);
+    gulp.watch('src/sass/**/*.scss', ['styles']);
 });
