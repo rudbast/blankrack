1733fbd78a9763cf24b351b0d91fe0a23409fb90
Rudolf <rudolf_bast@live.com>
2015-09-26 21:25:41 +0700

    Move assets/img/ to src/img & add image minifier to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('styles', function() {
     gulp.src('src/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('assets/css/'));
+});
+
+gulp.task('imagemin', function () {
+    gulp.src('src/img/**.*')
+        .pipe(imagemin({
+            progressive: true
+        })).pipe(gulp.dest('assets/img/'));
 });
 
 gulp.task('default', function() {
